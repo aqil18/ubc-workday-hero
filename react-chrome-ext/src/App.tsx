2ccb4d3e9835c70cd2787f7e6768172678a2d881
aqilf <afaizala@students.cs.ubc.ca>
2024-05-19 01:43:48 -0700

    got the url budddd

--- react-chrome-ext/src/App.tsx
+++ react-chrome-ext/src/App.tsx
@@ -1,14 +1,18 @@
+/* global chrome */
 import React from 'react';
 import './App.css';
 function App() {
 
     const handleClick = () => {
-        console.log("HSDSJNFSDF")
+        window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs: any[]) => {
+            const currentTab = tabs[0];
+            alert(`Current URL: ${currentTab.url}`);
+        });
     };
 
     return (
       <div className="App">
-          <button type="button" onClick={handleClick}>Click Me</button>
+          <button type="button" onClick={handleClick}>Get Current URL</button>
       </div>
     );
 }
