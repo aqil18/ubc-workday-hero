158d26a5e4f0179efef279a288d0ae294a81b467
aqilf <afaizala@students.cs.ubc.ca>
2024-05-19 01:57:48 -0700

    got course details.

--- react-chrome-ext/src/App.tsx
+++ react-chrome-ext/src/App.tsx
@@ -1,12 +1,26 @@
-/* global chrome */
 import React from 'react';
 import './App.css';
-function App() {
 
+function getCourseString(url : string) : string {
+    const urlObj = new URL(url);
+    const params = new URLSearchParams(urlObj.search);
+
+    const dept = params.get('dept');
+    const course = params.get('course');
+
+    return `${dept}/${course}`
+}
+
+
+function App() {
     const handleClick = () => {
         window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs: any[]) => {
             const currentTab = tabs[0];
+            const courseString = getCourseString(currentTab.url);
+            console.log(courseString);
+            //const courseJson
             alert(`Current URL: ${currentTab.url}`);
+
         });
     };
 
