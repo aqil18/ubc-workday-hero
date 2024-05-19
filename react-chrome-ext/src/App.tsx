57bd96e80a269bf810e96cd18ad36f0511a07c88
aqilf <afaizala@students.cs.ubc.ca>
2024-05-19 16:51:58 -0700

    COMPLETED NETWORK REQUEST TO UBC GRADES

--- react-chrome-ext/src/App.tsx
+++ react-chrome-ext/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+
+const UBCGRADES: string  = 'https://ubcgrades.com/api/v3/course-statistics/UBCV/'
 function getCourseString(url : string) : string {
     const urlObj = new URL(url);
     const params = new URLSearchParams(urlObj.search);
@@ -12,13 +14,29 @@ function getCourseString(url : string) : string {
 }
 
 
+function getCourseJson(courseString : string) : any {
+    fetch(UBCGRADES + courseString)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
 function App() {
     const handleClick = () => {
         window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs: any[]) => {
             const currentTab = tabs[0];
             const courseString = getCourseString(currentTab.url);
             console.log(courseString);
-            //const courseJson
+            const courseJson : any = getCourseJson(courseString);
             alert(`Current URL: ${currentTab.url}`);
 
         });
@@ -26,7 +44,7 @@ function App() {
 
     return (
       <div className="App">
-          <button type="button" onClick={handleClick}>Get Current URL</button>
+          <button type="button" onClick={handleClick}>Get Course Grades</button>
       </div>
     );
 }
