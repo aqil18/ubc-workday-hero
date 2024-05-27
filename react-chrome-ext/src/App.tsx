472b50db8d0a07b7e5a2e493d0a54f6669e8506e
aqilf <afaizala@students.cs.ubc.ca>
2024-05-26 23:21:00 -0700

    Broke the code for what

--- react-chrome-ext/src/App.tsx
+++ react-chrome-ext/src/App.tsx
@@ -31,14 +31,40 @@ function getCourseJson(courseString : string) : any {
         });
 }
 
+function DOMtoString(selector: any) {
+    if (selector) {
+        selector = document.querySelector(selector);
+        if (!selector) return "ERROR: querySelector failed to find node"
+    } else {
+        selector = document.documentElement;
+    }
+    return selector.outerHTML;
+}
 function App() {
     const handleClick = () => {
-        window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs: any[]) => {
-            const currentTab = tabs[0];
-            const courseString = getCourseString(currentTab.url);
-            console.log(courseString);
-            const courseJson : any = getCourseJson(courseString);
-            alert(`Current URL: ${courseJson.average_past_5_yrs}`);
+        // window.chrome.tabs.query({ active: true, currentWindow: true }, (tabs: any[]) => {
+        //     const currentTab = tabs[0];
+        //     const courseString = getCourseString(currentTab.url);
+        //     console.log(courseString);
+        //     const courseJson : any = getCourseJson(courseString);
+        //     alert(`Current URL: ${courseJson.average_past_5_yrs}`);
+        // });
+
+        window.chrome.tabs.query({ active: true, currentWindow: true }).then(function (tabs: any[]) {
+            var activeTab = tabs[0];
+            var activeTabId = activeTab.id;
+
+            return window.chrome.scripting.executeScript({
+                target: { tabId: activeTabId },
+                injectImmediately: true,  // uncomment this to make it execute straight away, other wise it will wait for document_idle
+                func: DOMtoString,
+                args: ['bodyddf']  // you can use this to target what element to get the html for
+            });
+
+        }).then(function (results: { result: any; }[]) {
+            alert(results[0].result);
+        }).catch(function (error: { message: string; }) {
+            alert(error.message);
         });
     };
 
