c15ab67fa56965e32a56f4da7ed41a2b1174c818
aqilf <aqilfaizal2004@gmail.com>
2024-06-02 02:15:33 -0700

    refactored backend

--- react-chrome-ext/src/App.tsx
+++ react-chrome-ext/src/App.tsx
@@ -2,22 +2,9 @@ import React from 'react';
 import './App.css';
 
 const UBCGRADES: string  = 'https://ubcgrades.com/api/v3/course-statistics/UBCV/'
-// function getCourseString(url : string) : string {
-//     const urlObj = new URL(url);
-//     const params = new URLSearchParams(urlObj.search);
-//
-//     const dept = params.get('dept');
-//     const course = params.get('course');
-//
-//     return `${dept}/${course}`
-// }
 
-function getCourseString(rawString : string) : string {
-    const dept = rawString.substring(0, 4);
-    const course = rawString.substring(7, 10);
 
-    return `${dept}/${course}`
-}
+
 
 
 
@@ -38,41 +25,64 @@ function getCourseJson(courseString : string) : any {
         });
 }
 
+async function getPageText() {
+    const [tab] = await window.chrome.tabs.query({active: true, currentWindow: true});
+    let result;
+    try {
+        [{result}] = await window.chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            func: () => document.documentElement.innerText,
+        });
+        return result;
+    } catch (e) {
+        document.body.textContent = 'Cannot access page';
+        return;
+    }
+}
+
+function getProfString(pageText : string) : string {
+    const rawProfString = pageText.match(/Instructor Teaching\n(.+?)\n/);
+
+    if (rawProfString) {
+        const instructorName = rawProfString[1].trim();
+        console.log(instructorName);
+        return instructorName;
+    } else {
+        console.log("Prof not found");
+        return " Prof Not Found";
+    }
+}
+
+function getCourseString(pageText : string) : string {
+    
+    const rawCourseStringArr = pageText.match(/\b[A-Z]{2,4}_V\s*\d{3}\b/g);
+    if (rawCourseStringArr) {
+        const rawCourseString = rawCourseStringArr[0]
+        const dept = rawCourseString.substring(0, 4);
+        const course = rawCourseString.substring(7, 10);
+
+        return `${dept}/${course}`;
+    } else {
+        console.log("Course not found");
+        return "Course Not Found";
+
+    }
+}
+
+function processCourseString(courseString:string) {
+    const courseJson = JSON.parse(getCourseJson(courseString));
+    alert("Course Average for " + courseString + ": " + courseJson.average)
+};
 
 function App() {
     const  handleClick = async () => {
-        const [tab] = await window.chrome.tabs.query({active: true, currentWindow: true});
-        console.log(tab.id);
-        let result;
-        try {
-            [{result}] = await window.chrome.scripting.executeScript({
-                target: {tabId: tab.id},
-                func: () => document.documentElement.innerText,
-            });
-        } catch (e) {
-            document.body.textContent = 'Cannot access page';
-            return;
-        }
-        console.log(result);
-        var rawString = result.match(/\b[A-Z]{2,4}_V\s*\d{3}\b/g);
+        const  pageText  = await getPageText();
 
-        // Regular expression to find the instructor's name
-        const instructorPattern = /Instructor Teaching\n(.+?)\n/;
+        const profString = getProfString(pageText)
+        const courseString = getCourseString(pageText)
 
-        // Find the match in the text
-        const match = result.match(instructorPattern);
-
-        if (match) {
-            const instructorName = match[1].trim();
-            console.log(instructorName);
-        } else {
-            console.log("Instructor not found");
-        }
-        console.log(rawString[0]);
-        const courseString = getCourseString(rawString[0])
-        console.log(courseString);
-        const courseJson = JSON.parse(getCourseJson(courseString));
-        alert("Course Average for " + rawString + ": " + courseJson.average);
+        processCourseString(courseString);
+        
     };
 
     return (
