0a90a2572380091174f8ab41df7c5ecf93106aea
aqilf <aqilfaizal2004@gmail.com>
2024-07-17 16:44:10 -0700

    creating a flask app

--- react-chrome-ext/src/App.tsx
+++ react-chrome-ext/src/App.tsx
@@ -1,9 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from 'react';
 import './App.css';
-const UBCGRADES: string  = 'https://ubcgrades.com/api/v3/course-statistics/UBCV/'
-let courseJson;
-let profUrl;
+const UBCGRADES: string  = 'https://ubcgrades.com/api/v3/course-statistics/UBCV/';
+import axios from axios;
 
 
 async function getPageText() {
@@ -79,16 +78,19 @@ async function processCourseString(courseString: string) {
     }
 }
 function processProfString(profString : string) {
-    let firstName = profString.trim().split(" ")[0]
-    let lastName = profString.trim().split(" ")[1]
-    let url = "";
+    // let firstName = profString.trim().split(" ")[0]
+    // let lastName = profString.trim().split(" ")[1]
+    // let url = "";
     
-    if (profString.length > 1) {
-        url = `<a href=https://www.ratemyprofessors.com/search/professors/1413?q=
-        ${lastName.toLowerCase()}%20${firstName.toLowerCase()}></a>`;
-    }
+    // if (profString.length > 1) {
+    //     url = `<a href=https://www.ratemyprofessors.com/search/professors/1413?q=
+    //     ${lastName.toLowerCase()}%20${firstName.toLowerCase()}></a>`;
+    // }
     
-    return url;
+    // return url;
+
+    let courseDict: Record<string, number> = {};
+
 }
 
 function App() {
@@ -96,8 +98,12 @@ function App() {
     const [average5, setAverage5] = useState("N/A");
     const [max, setMax] = useState("N/A");
     const [min, setMin] = useState("N/A");
+    
     const [profUrl, setProfUrl] = useState("N/A");
-
+    const [profRating, setProfRating] = useState("N/A");
+    const [profDiff, setProfDiff] = useState("N/A");
+    const [profReviews, setProfReviews] = useState("N/A");
+    const [profWouldTake, setProfWouldTake] = useState("N/A");
 
     useEffect(() => {
         const fetchData = async () => {
@@ -112,14 +118,11 @@ function App() {
             setMax(courseDict["max"]);
             setMin(courseDict["min"]);
 
-            const profUrl = processProfString(profString);
+            const profDict = processProfString(profString);
             
-            
-            setProfUrl(profUrl);
+        
 
-            
-            console.log(courseDict, profUrl);
-        };
+            };
 
         fetchData();
     }, []); // Empty dependency array ensures this runs only once after the initial render
@@ -156,8 +159,23 @@ function App() {
             </div>
             <div className='content-bottom'>
                 <section className="bottom-section">
-                <h1 className='style2'>Prof Section</h1>
-                <p className='style1'>RateMyProf Link: {profUrl}</p>
+                    <h1 className='style2'>Professor Details</h1>
+                    
+                    <div className='flexbox-container'>
+                        <div>
+                            <p className='style1'>Overall: {average}</p>
+                        </div>
+                        <div>
+                            <p className='style1'>Past Years: {average5}</p>
+                        </div> 
+                        <div>
+                            <p className='style1'> Max: {max} </p>
+                        </div>
+                        <div>
+                            <p className='style1'>Min: {min}</p>
+                        </div>
+                    </div>
+
                 </section>
                 <section></section>
             </div>
---
+++ react-chrome-ext/src/rmp-app.py
@@ -0,0 +1,34 @@
+import ratemyprofessor
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+school = ratemyprofessor.get_school_by_name("University of British Columbia")
+
+@app.route('/process', methods=['POST'])
+def process_input():
+    profName = request.json
+    # Process the input data as needed
+    response =  getFromRmp(profName)
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+def getFromRmp(profName):
+    professor = ratemyprofessor.get_professor_by_school_and_name(school, profName)
+    if professor is not None:
+        print("%s works in the %s Department of %s." % (professor.name, professor.department, professor.school.name))
+        print("Rating: %s / 5.0" % professor.rating)
+        print("Difficulty: %s / 5.0" % professor.difficulty)
+        print("Total Ratings: %s" % professor.num_ratings)
+        if professor.would_take_again is not None:
+            print(("Would Take Again: %s" % round(professor.would_take_again, 1)) + '%')
+        else:
+            print("Would Take Again: N/A")
+    return {"name" : professor.name,
+            "department": professor.department,
+            "school": professor.school.name,
+             "rating" : professor.rating,
+            "difficulty" : professor.difficulty,
+            "num_ratings": professor.num_ratings,
+            "would_take_again"  : professor.would_take_again}
